16622bfc694a7ece4edbde266113d71f66048048
Gaspard Deremble <deremblg@ige-calcul1.u-ga.fr>
2024-07-17 12:09:06 +0200

    first server sim

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J TestOceananigansCPU
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --account=meom
+
+#SBATCH --mem=10000
+
+#SBATCH --time=01:00:00
+#SBATCH --output results/logs/OceWind.%j.output
+#SBATCH --error  results/logs/OceWind.%j.error
+
+
+julia --project=. src/OceWind.jl
--- src/OceWind.jl
+++ src/OceWind.jl
@@ -32,13 +32,13 @@ const save_fig_path = "results/visuals/figs_"
 
 function main()
 
-	#run(sim_command)
+    run(sim_command)
 
     @info "Simulations completed!"
 
-    #FormatData.main(nb_cores, save_file_sim, save_file_data)
+    FormatData.main(nb_cores, save_file_sim, save_file_data)
 
-    PlotData.main(save_file_data, save_fig_path)
+    #PlotData.main(save_file_data, save_fig_path)
 
     @info "All done!"
 

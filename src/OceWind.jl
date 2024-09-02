b9abada4e9caab1a7fcbe57f444a389bcfaa0a26
Gaspartcho <g.deremble@laposte.net>
2024-09-02 17:25:31 +0200

    cleaned branch

--- src/OceWind.jl
+++ src/OceWind.jl
@@ -7,22 +7,11 @@ module OceWind
 using Logging
 
 
-# === Importing Sub-Scripts ===
-
-#include("format_data.jl")
-#include("plot_data.jl")
-
-
 # === Main variables ===
 
 const path_script_sim = "simulation/main_sim.jl"
 
 const sim_command = `$(Base.julia_cmd()) --project src/$path_script_sim`
-
-
-const save_file_sim = "results/data/ocean_wind_mixing_and_convection2542.nc"
-const save_file_data = "results/data/formated_data.nc"
-const save_fig_path = "results/visuals/figs_"
 
 
 # === Main Function
@@ -34,10 +23,6 @@ function main()
     end
 
     @info "Simulations completed!"
-
-    #FormatData.main(save_file_sim, save_file_data)
-
-    #PlotData.main(save_file_data, save_fig_path)
 
     @info "All done!"
 

55fe83f5d7ca02dd925700a981f9ac955ab4ac7b
Gaspartcho <g.deremble@laposte.net>
2024-09-02 17:27:06 +0200

    finished cleaning

--- src/OceWind.jl
+++ src/OceWind.jl
@@ -4,7 +4,6 @@ module OceWind
 
 # === Importing libraries ===
 
-using Logging
 using MPI
 
 
@@ -15,11 +14,6 @@ const nb_cores = 32
 const path_script_sim = "simulation/main_sim.jl"
 
 const sim_command = `$(mpiexec()) -n $nb_cores $(Base.julia_cmd()) --project src/$path_script_sim`
-
-
-const save_file_sim = "results/data/ocean_wind_mixing_and_convection_rank"
-const save_file_data = "results/data/formated_data.nc"
-const save_fig_path = "results/visuals/figs_"
 
 
 # === Main Function
